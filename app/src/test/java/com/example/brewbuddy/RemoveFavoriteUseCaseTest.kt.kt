c113383f47c89d3798b961dae1c52b84e28dad07
Youstina <Ypustina@gmail.com>
2025-12-14 19:01:53 +0200

    creatingScreens

---
+++ app/src/test/java/com/example/brewbuddy/RemoveFavoriteUseCaseTest.kt.kt
@@ -0,0 +1,20 @@
+package com.example.brewbuddy
+
+import com.example.brewbuddy.domain.repository.FavoritesRepository
+import com.example.brewbuddy.domain.usecase.favorites.RemoveFavoriteUseCase
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+class RemoveFavoriteUseCaseTest {
+
+    private val repo = mock<FavoritesRepository>()
+    private val useCase = RemoveFavoriteUseCase(repo)
+
+    @Test
+    fun `should remove favorite by id`() = runTest {
+        useCase(10)
+        verify(repo).removeFavorite(10)
+    }
+}
